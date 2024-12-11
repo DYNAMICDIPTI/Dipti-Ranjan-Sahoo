562527eeeb9c759e86d06f4a3157e57ca56f6a3d
DYNAMICDIPTI <diptiranjansahooitm0020@gmail.com>
2024-12-11 22:30:53 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,4 @@
+a = 50
+b = 6
+print("the answer is :", a/b)
+print("Remainder when a is divided by b is :", a % b)
--- problem4.py
+++ problem4.py
@@ -1,3 +1,4 @@
-import os 
-directory_path = '\'
- 
+a = int(input("enter number 1:"))
+b = int(input("enter number 2:"))
+
+print("a is greater than b is ",a>b)
